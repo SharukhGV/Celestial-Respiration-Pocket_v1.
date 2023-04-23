c6c852ffa8fbddcb3d8c17891629e033b714cb36
SharukhGV <sharukhvelupillai@pursuit.org>
2023-04-23 13:38:15 -0400

    Initial commit

---
+++ app/src/main/assets/app.js
@@ -0,0 +1,127 @@
+
+
+let add=document.getElementById('increment')
+let remove = document.getElementById('decrement')
+let int = document.getElementById('plants');
+let integer = 0;
+add.addEventListener('click',function(){
+    integer +=1;
+    int.innerHTML = integer;
+})
+
+remove.addEventListener('click', function(){
+integer-= 1;
+int.innerHTML=integer;
+})
+
+
+
+let addhealth=document.getElementById('incrementhealth')
+let removehealth = document.getElementById('decrementhealth')
+let inthealth = document.getElementById('health');
+let integerhealth = 0;
+addhealth.addEventListener('click',function(){
+    integerhealth +=1;
+    inthealth.innerHTML = integerhealth;
+})
+
+removehealth.addEventListener('click', function(){
+    integerhealth-= 1;
+inthealth.innerHTML=integerhealth;
+})
+
+let addWATER=document.getElementById('incrementWATER')
+let removeWATER = document.getElementById('decrementWATER')
+let intWATER = document.getElementById('water');
+let integerWATER = 0;
+addWATER.addEventListener('click',function(){
+    integerWATER +=1;
+    intWATER.innerHTML = integerWATER;
+})
+
+removeWATER.addEventListener('click', function(){
+    integerWATER-= 1;
+intWATER.innerHTML=integerWATER;
+})
+
+
+
+let addCOT=document.getElementById('incrementCOT')
+let removeCOT = document.getElementById('decrementCOT')
+let intCOT = document.getElementById('cotton');
+let integerCOT = 0;
+addCOT.addEventListener('click',function(){
+    integerCOT +=1;
+    intCOT.innerHTML = integerCOT;
+})
+
+removeCOT.addEventListener('click', function(){
+    integerCOT-= 1;
+intCOT.innerHTML=integerCOT;
+})
+
+
+
+let addAPPLE=document.getElementById('incrementAPPLE')
+let removeAPPLE = document.getElementById('decrementAPPLE')
+let intAPPLE = document.getElementById('apple');
+let integerAPPLE = 0;
+addAPPLE.addEventListener('click',function(){
+    integerAPPLE +=1;
+    intAPPLE.innerHTML = integerAPPLE;
+})
+
+removeAPPLE.addEventListener('click', function(){
+    integerAPPLE-= 1;
+intAPPLE.innerHTML=integerAPPLE;
+})
+
+
+let addore=document.getElementById('incrementore')
+let removeore = document.getElementById('decrementore')
+let intore = document.getElementById('ore');
+let integerore = 0;
+addore.addEventListener('click',function(){
+    integerore +=1;
+    intore.innerHTML = integerore;
+})
+
+removeore.addEventListener('click', function(){
+    integerore-= 1;
+intore.innerHTML=integerore;
+})
+
+
+
+
+let addwood=document.getElementById('incrementwood')
+let removewood = document.getElementById('decrementwood')
+let intwood = document.getElementById('wood');
+let integerwood = 0;
+addwood.addEventListener('click',function(){
+    integerwood +=1;
+    intwood.innerHTML = integerwood;
+})
+
+removewood.addEventListener('click', function(){
+    integerwood-= 1;
+intwood.innerHTML=integerwood;
+})
+
+
+
+
+let addstone=document.getElementById('incrementstone')
+let removestone = document.getElementById('decrementstone')
+let intstone = document.getElementById('stone');
+let integerstone = 0;
+addstone.addEventListener('click',function(){
+    integerstone +=1;
+    intstone.innerHTML = integerstone;
+})
+
+removestone.addEventListener('click', function(){
+    integerstone-= 1;
+intstone.innerHTML=integerstone;
+})
+
